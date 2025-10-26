dfe38cd7c90737aba8a40c5ec208f1cab8dfa3b0
Victor Pandakov <85847075+pandakov@users.noreply.github.com>
2025-10-26 19:02:17 +0300

    apply linting

--- src/titanic.py
+++ src/titanic.py
@@ -1,13 +1,6 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-from sqlalchemy import create_engine
-
-DB_USER = st.secrets["postgres"]["user"]
-DB_PASS = st.secrets["postgres"]["password"]
-DB_HOST = st.secrets["postgres"]["host"]
-DB_PORT = st.secrets["postgres"]["port"]
-DB_NAME = st.secrets["postgres"]["dbname"]
 
 st.title("Titanic Data Visualization")
 
@@ -17,18 +10,11 @@ st.set_page_config(
     layout="wide",
 )
 
-@st.cache_resource
-def get_engine():
-    conn_str = f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-    return create_engine(conn_str)
-
-
-engine = get_engine()
 
 @st.cache_data
 def load_data():
-    query = "SELECT * FROM public.titanic"
-    return pd.read_sql(query, engine)
+    return pd.read_parquet("data/titanic.parquet")
+
 
 df = load_data()
 
@@ -46,18 +32,34 @@ avg_fare_f = df["fare"].mean() if "fare" in df.columns else None
 
 with col_kpi1:
     st.metric("Пассажиров", total_all, delta=total_all)
+
 with col_kpi2:
     if surv_rate_f is not None:
-        st.metric("Доля выживших", f"{surv_rate_f:,.1%}",
-                  delta=None if surv_rate_all is None else f"{(surv_rate_f - surv_rate_all):.1%}")
+        st.metric(
+            "Доля выживших",
+            f"{surv_rate_f:,.1%}",
+            delta=(
+                None
+                if surv_rate_all is None
+                else f"{(surv_rate_f - surv_rate_all):.1%}"
+            ),
+        )
 with col_kpi3:
     if avg_age_f is not None:
-        st.metric("Средний возраст", f"{avg_age_f:,.1f}",
-                  delta=None if avg_age_all is None else f"{(avg_age_f - avg_age_all):.1f}")
+        st.metric(
+            "Средний возраст",
+            f"{avg_age_f:,.1f}",
+            delta=None if avg_age_all is None else f"{(avg_age_f - avg_age_all):.1f}",
+        )
 with col_kpi4:
     if avg_fare_f is not None:
-        st.metric("Средний тариф", f"{avg_fare_f:,.2f}",
-                  delta=None if avg_fare_all is None else f"{(avg_fare_f - avg_fare_all):.2f}")
+        st.metric(
+            "Средний тариф",
+            f"{avg_fare_f:,.2f}",
+            delta=(
+                None if avg_fare_all is None else f"{(avg_fare_f - avg_fare_all):.2f}"
+            ),
+        )
 
 
 st.subheader("Preview of Dataset")
@@ -92,5 +94,7 @@ fig3d = px.scatter_3d(
 st.plotly_chart(fig3d, use_container_width=True)
 
 st.subheader("Survival Distribution by Sex")
-fig_hist = px.histogram(df, x="sex", color="survived", barmode="group", title="Survival by Gender")
+fig_hist = px.histogram(
+    df, x="sex", color="survived", barmode="group", title="Survival by Gender"
+)
 st.plotly_chart(fig_hist, use_container_width=True)
